cf932725420af2532d4e48ea9c06ab1ba7963b52
Apoorvsinghai <apoorv.singhai722@gmail.com>
2025-12-29 03:21:45 +0530

    renamed project

--- demo/src/main/java/com/cartoffer/demo/controller/SegmentResponse.java
+++
@@ -1,8 +0,0 @@
-package com.cartoffer.demo.controller;
-
-import lombok.Data;
-
-@Data
-public class SegmentResponse {
-    private String segment;
-}
--- demo/src/test/java/com/cartoffer/demo/CartOfferApplicationTests.java
+++
@@ -1,6 +0,0 @@
-package com.cartoffer.demo;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class CartOfferApplicationTests { }
